57cb1089f1d83381e088d50cdbe12f000975e0ac
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-27 13:23:41 -0500

    Testing matlab-VSCode integration

--- matlab_question_1.m
+++ matlab_question_1.m
@@ -1,12 +1,12 @@
-n=1:0.001:3;
-X =@(n) sin (2*pi*n);
-x=X(n);
-% dilation 
-xd=X(3.*n);
+n = 1:0.001:3;
+X = @(n) sin (2 * pi * n);
+x = X(n);
+% dilation
+xd = X(3 .* n);
 %translation;
-xt=X(3.*n+3);
-plot(n,x,'r',n,xd,'b',n,xt,'go');
-legend('x','xd','xt','Location','south');
+xt = X(3 .* n + 3);
+plot(n, x, 'r', n, xd, 'b', n, xt, 'go');
+legend('x', 'xd', 'xt', 'Location', 'south');
 grid on;
 title('X versus Time plots - original, dilation, and translation');
 xlabel('time (sec)');
